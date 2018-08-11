48bc41ac058fe1e78569c18387eb3b4ce7a9a3f7
Matthew McCord <mattm.eternal@gmail.com>
2018-08-11 16:07:06 -0500

    Create item_routes file as part of notes routes refactor

---
+++ app/routes/item_routes.js
@@ -0,0 +1,82 @@
+const ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+    app.delete('/items/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id' : new ObjectID(id) };
+
+        db.collection('items').remove(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+
+                return;
+            }
+
+            res.send(`item ${id} deleted!`);
+        })
+    })
+
+    app.get('/items', (req, res) => {
+        db.collection('items').find().toArray((err, items) => {
+            if (err) {
+                res.send({ 'error': 'An error occured' });
+
+                return;
+            }
+
+            res.send(items)
+        })
+    })
+
+    app.get('/items/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('items').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+
+                return;
+            }
+
+            res.send(item);
+        })
+    })
+
+    app.post('/items', (req, res) => {
+        const item = {
+            description: req.body.description,
+            title: req.body.title,
+        };
+
+        db.collection('items').insert(item, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+
+                return;
+            }
+
+            console.log(result.ops);
+            res.send(result.ops[0]);
+        })
+    });
+
+    app.put('/items/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const item = {
+            text: req.body.body,
+            title: req.body.title,
+        };
+
+        db.collection('items').update(details, item, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+
+                return;
+            }
+
+            res.send(item);
+        });
+    });
+};
